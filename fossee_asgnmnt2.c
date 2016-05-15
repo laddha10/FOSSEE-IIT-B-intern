62e90ef8b01b1cdcf9846161180b4c87d3fd3386
Sankalp gupta <sankalp341@gmail.com>
2016-05-15 21:00:47 +0530

    Add files via upload

---
+++ fossee_asgnmnt2.c
@@ -0,0 +1,13 @@
+     #include "fossee_asgnmnt2.h"
+     
+     void addme(const real a[5], const real b[10], real c[2])
+     {
+       int z;
+       int y;
+       for (z = 0; z < 2; z++) {
+         c[z] = 0;
+         for (y = 0; y < 5; y++) {
+          c[z] += a[y] + b[y];
+        }
+      }
+    }
